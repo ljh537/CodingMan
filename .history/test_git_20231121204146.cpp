aad37dac74985cff5a0540e6f0b7419bb356ff95
OctoPlus <1954466134@qq.com>
2023-11-21 20:44:49 +0800

    001
    This is the first code I write down in order to learn git!

---
+++ .history/test_git_20231121204146.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 8;
+    cout << "This is Ross!" << endl
+         << n << endl;
+}
---
+++ .history/test_git_20231121204149.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 8;
+    cout << "This is Ross!" << endl
+         << n << endl;
+    return 0;
+}
